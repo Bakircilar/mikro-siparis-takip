f662e2ccd063a8aaf04b7320e8b4c244d76fdc4f
Bakircilar <ensarucarer@bakircilargrup.com>
2025-03-10 15:58:42 +0300

    1. düzeltme

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Supabase bağlantı bilgileri
-const supabaseUrl = 'https://ouslqozvofsxyhplfged.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im91c2xxb3p2b2ZzeHlocGxmZ2VkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE2MDIyMzksImV4cCI6MjA1NzE3ODIzOX0.v9uKw8zE5Xfa97w4EjPnvjpAMR_j2B54o6TpL_E87xQ';
+const supabaseUrl = process.env.https://ouslqozvofsxyhplfged.supabase.co;
+const supabaseKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im91c2xxb3p2b2ZzeHlocGxmZ2VkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE2MDIyMzksImV4cCI6MjA1NzE3ODIzOX0.v9uKw8zE5Xfa97w4EjPnvjpAMR_j2B54o6TpL_E87xQ;
 
 // Supabase istemcisini oluştur
 const supabase = createClient(supabaseUrl, supabaseKey);
